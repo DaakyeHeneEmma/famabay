0a99e5273a7098468aacd777e5a4adefe227205e
DaakyeHeneEmma <emmaatta.me@yahoo.com>
2020-11-13 02:07:49 +0000

    set middileware and changes in package.json

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
+
 const express = require('express')
 
 const app = express();
 const PORT = process.env.PORT || 2000
 
-app.get('/', (req,res)=>{
-    res.send('hello from here');
-    res.end();
+express.json([
+    limit = '95kb'
+])
 
+app.get('/', (_,res,next)=>{
+    res.status(200).contentType('application/json')
+    .cookie('rememberme', '1', 
+    {expires:new Date(Date.now() + 4000), httpOnly: true});
+    res.send('hello from here! coming from vsCode');
+    res.end();
+    next();
 })
 
 app.listen(PORT, ()=>{
-    console.log('server connected');
+    console.table([
+        {path: `http://127.0.0.1`, port: `${PORT}`,index: 01}
+    ])
 })
