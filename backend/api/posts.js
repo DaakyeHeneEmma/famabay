2ef6406b7c56eb255fa817ed6d963a25bcd53736
DaakyeHeneEmma <emmaatta.me@yahoo.com>
2022-01-09 00:12:41 +0000

    tried hands on authentication

--- backend/api/posts.js
+++ backend/api/posts.js
@@ -1,14 +1,22 @@
 const { router, UserSchema, app } = require('../middlewares/postRouter')
- 
+ const bcrypt = require("bcrypt")
 
  const posts = router.post("/", async(req,res)=>{
+    try {
+      
       const {name,age,course}= req.body
-      const data = await new UserSchema({ name,age,course})
+      const hash = await bcrypt.hash(course, 10)
+      console.log(hash)
+      const data = await new UserSchema({ name,age,hash})
+      
       data.save().then((result)=>{
         console.log(result)
         res.send(result)
         console.log('item inserted')
       })
-    })
-
+ 
+    } catch(error) {
+      console.log(error.massage);
+    }
+  })
 module.exports = posts;
